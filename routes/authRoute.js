71f89a797dff574de7280ea50d1b003bfdf169b9
EubySan <eubysanchez14@gmail.com>
2022-03-05 12:05:14 -0500

    se le quita el value al password

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,8 +4,8 @@ const AuthController = require('../controllers/authController');
 const router = express.Router();
 const authController = new AuthController();
 
-router.get('/', authController.getLoginView)
-router.get('/signup', authController.getSignupView)
-router.post('/signup', authController.signUp)
+router.get('/', authController.getLoginView);
+router.get('/signup', authController.getSignupView);
+router.post('/signup', authController.signUp);
 
-module.exports = router
+module.exports = router;
